ca5ab3d4c3d072209f1d8af4e139f969277af827
Ben W <mrbrwells@gmail.com>
2016-04-17 11:56:37 -0400

    syn appendfile as well

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var numCommits = Math.floor(Math.random() * 15) + 1;
 console.log('numCommits', numCommits);
 exec('git checkout test', {async: false});
 
-return;
 for (i = 0; i <= numCommits; i++) {
   var numWords = Math.floor(Math.random() * 7) + 1;
   console.log('numWords', numWords);
@@ -20,9 +19,12 @@ for (i = 0; i <= numCommits; i++) {
   }
   sentence = sentence.trim();
   console.log('appending sentence', sentence + '\n');
+  fs.appendFileSync('52331.txt', sentence);
+/*
   fs.appendFile('52331.txt', sentence, function (err) {
     echo('error appending', err);
   });
+*/
   exec('git add 52331.txt', { async: false });
   exec('git commit -m "' + newWord.generate() + '"', { async: false });
 
