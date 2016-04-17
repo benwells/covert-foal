e5c5d251845b8026480bc79643fc0704d56cc48e
Ben W <mrbrwells@gmail.com>
2016-04-17 12:13:38 -0400

    commits have to be pushed to master to count

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ var fs = require('fs');
 var numCommits = Math.floor(Math.random() * 15) + 1;  
 var newWord = new Rwg();
 
-/**
-  This is a a little lazy, but checkout to a new branch first.
-  if branch already exists, then -b will fail.  Then you can 
-  checkout to the existing branch.
-  The vice versa scenario works as well.  If test doesn't exist,
-  then -b will create new branch, then checkout without -b will
-  just tell you that you are already on that branch.
-*/
-syncExec('git checkout -b test');
-syncExec('git checkout test');
-
 // loop through commits
 for (i = 0; i < numCommits; i++) {
 
@@ -35,10 +24,10 @@ for (i = 0; i < numCommits; i++) {
   syncExec('git commit -m "' + newWord.generate() + '"');
 }
 
-syncExec('git push origin test');
-syncExec('git push --delete origin test');
-syncExec('git checkout master');
-syncExec('git branch -D test');
+syncExec('git push origin master');
+// syncExec('git push --delete origin test');
+// syncExec('git checkout master');
+// syncExec('git branch -D test');
 
 function syncExec(cmd) {
   exec(cmd, { async: false });
